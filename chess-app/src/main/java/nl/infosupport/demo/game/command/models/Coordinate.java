306cf654d6070462d31d4ad63a9057b028bda986
Raymond Phua <raymond.phua@infosupport.com>
2021-04-28 17:20:31 +0200

    query assignment

--- chess-app/src/main/java/nl/infosupport/demo/game/command/models/Coordinate.java
+++
@@ -1,9 +0,0 @@
-package nl.infosupport.demo.game.command.models;
-
-import lombok.Value;
-
-@Value
-public class Coordinate {
-    int x;
-    int y;
-}
--- chess-app/src/main/java/nl/infosupport/demo/matchmaker/query/controllers/InviteQueryController.java
+++ chess-app/src/main/java/nl/infosupport/demo/matchmaker/query/controllers/InviteQueryController.java
@@ -1,7 +1,6 @@
 package nl.infosupport.demo.matchmaker.query.controllers;
 
 import nl.infosupport.demo.matchmaker.query.readmodels.Invite;
-import nl.infosupport.demo.matchmaker.query.services.InviteQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +13,16 @@ import java.util.concurrent.ExecutionException;
 @RequestMapping("/invites")
 public class InviteQueryController {
 
-    private final InviteQueryService inviteQueryService;
-
-    @Autowired
-    public InviteQueryController(InviteQueryService inviteQueryService) {
-        this.inviteQueryService = inviteQueryService;
-    }
-
-    @GetMapping("")
-    public List<Invite> getInvites() throws ExecutionException, InterruptedException {
-        return inviteQueryService.findAllInvites();
-    }
+    //todo: implement when InviteQueryService has been implemented
+//    private final InviteQueryService inviteQueryService;
+//
+//    @Autowired
+//    public InviteQueryController(InviteQueryService inviteQueryService) {
+//        this.inviteQueryService = inviteQueryService;
+//    }
+//
+//    @GetMapping("")
+//    public List<Invite> getInvites() throws ExecutionException, InterruptedException {
+//        return inviteQueryService.findAllInvites();
+//    }
 }
